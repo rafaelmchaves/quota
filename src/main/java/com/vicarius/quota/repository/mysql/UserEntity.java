8c9785c4e688458843e0f30a9a3cb7c06d17e31c
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-03-28 16:59:29 -0300

    Create the user repository

---
+++ src/main/java/com/vicarius/quota/repository/mysql/UserEntity.java
@@ -0,0 +1,26 @@
+package com.vicarius.quota.repository.mysql;
+
+import com.vicarius.quota.model.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "last_login_time_utc")
+    private LocalDateTime lastLoginTimeUtc;
+    private LocalDateTime creation;
+    private LocalDateTime update;
+    private Status status;
+
+}
