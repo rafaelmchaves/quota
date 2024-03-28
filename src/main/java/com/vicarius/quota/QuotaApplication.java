efcdf01150eb8025ad40766f245393863ea2b009
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-03-28 13:07:13 -0300

    Initializing the project with mysql configuration

---
+++ src/main/java/com/vicarius/quota/QuotaApplication.java
@@ -0,0 +1,13 @@
+package com.vicarius.quota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicarius/quota/QuotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicarius.quota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
