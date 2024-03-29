138fa9baa0433bac320bd449facefd345a18dbfa
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-03-29 17:09:13 -0300

    Implementing Quota service

--- src/main/java/com/vicarius/quota/services/QuotaService.java
+++ src/main/java/com/vicarius/quota/services/QuotaService.java
@@ -1,4 +1,7 @@
 package com.vicarius.quota.services;
 
 public interface QuotaService {
+
+    void consumeQuota(String userId);
+
 }
--- src/main/java/com/vicarius/quota/services/impl/QuotaServiceImpl.java
+++ src/main/java/com/vicarius/quota/services/impl/QuotaServiceImpl.java
@@ -1,6 +1,26 @@
 package com.vicarius.quota.services.impl;
 
+import com.vicarius.quota.repository.DatabaseStrategy;
 import com.vicarius.quota.services.QuotaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class QuotaServiceImpl implements QuotaService {
+
+    private final DatabaseStrategy databaseStrategy;
+
+    @Override
+    public void consumeQuota(String userId) {
+        final var user = this.databaseStrategy.getDatabase().get(UUID.fromString(userId));
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
+
+
+    }
 }
