75305aa9ff23c1a32a446bba7c218d8f27867029
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-03-30 08:18:40 -0300

    implement simulating an implementation of operations in Elastic search

--- src/main/java/com/vicarius/quota/repository/elastic/ElasticImplementation.java
+++ src/main/java/com/vicarius/quota/repository/elastic/ElasticImplementation.java
@@ -5,40 +5,42 @@ import com.vicarius.quota.repository.UserBoundary;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
 @Service(ElasticImplementation.IMPLEMENTATION_ID)
 public class ElasticImplementation implements UserBoundary {
+
+    private final Map<String, User> registers = new HashMap<>();
+
     public static final String IMPLEMENTATION_ID = "ElasticImplementation";
 
     @Override
     public User save(User user) {
-
-        log.info("User was saved");
-        return null;
+        registers.put(user.getId(), user);
+        return user;
     }
 
     @Override
     public User get(UUID id) {
-        log.info("User was found");
-        return null;
+        return registers.get(id.toString());
     }
 
     @Override
     public User update(User user) {
-        log.info("User was updated");
-        return null;
+        return save(user);
     }
 
     @Override
     public void delete(UUID id) {
-        log.info("User was deleted");
+        registers.remove(id.toString());
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return registers.values().stream().toList();
     }
 }
